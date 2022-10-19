b689474173799f15d90155a873f6944cae607d87
sinashish <ashishsinha108@gmail.com>
2022-10-19 14:06:37 -0700

    adds setup script for new machines; ssh config

---
+++ setup.sh
@@ -0,0 +1,35 @@
+
+# install omz
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+
+# install zsh plugins for autocompletion and syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+
+# install vim-plug for vim and nvim (if present on servers)
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# now connect the dotfiles to .dotfiles on current system
+
+rsync -avz nvim/init.vim ~/.config/nvim/init.vim
+
+rsync -avz nvim/init.vim ~/.vimrc
+
+rsync -avz .zshrc ~/.zshrc
+
+rsync -avz oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
+
+echo "Now install the vim plugins by opening vim"
+
+echo "Then again execute this command: source ~/.zshrc "
+
+source ~/.zshrc
