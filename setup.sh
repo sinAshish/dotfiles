a4b83f1f19ec00ff5926cf1b62268edb46d26c9e
asa409 <asa409@cs-venus-13.cmpt.sfu.ca>
2024-01-09 12:42:53 -0800

    update on cs-venus-13

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 
 
 # install vim-plug for vim and nvim (if present on servers)
-<<<<<<< HEAD
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -23,7 +22,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 rsync -avz nvim/init.vim ~/.config/nvim/init.vim
 
-=======
 if [ -d "~/.vim" ];then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -41,18 +39,14 @@ fi
 
 # now connect the dotfiles to .dotfiles on current system
 
->>>>>>> 0a3b81a9d552be79531ff39cb6e8a3914005b2f7
 rsync -avz nvim/init.vim ~/.vimrc
 
 rsync -avz .zshrc ~/.zshrc
 
 rsync -avz oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
 
-<<<<<<< HEAD
-=======
 rsync -avz ./robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 
->>>>>>> 0a3b81a9d552be79531ff39cb6e8a3914005b2f7
 echo "Now install the vim plugins by opening vim"
 
 echo "Then again execute this command: source ~/.zshrc "
